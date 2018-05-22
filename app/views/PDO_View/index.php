d015f115e7993132f23ed12303324243c13a0bf4
imamkurniawan <imamkurniawan.ntb@gmail.com>
2018-05-23 04:39:29 +0800

    Update index.php

--- app/views/PDO_View/index.php
+++ app/views/PDO_View/index.php
@@ -6,9 +6,18 @@ echo "<h4>This is index in PDO View </h4>";
 
 // Print array
 // print_r($data);
+// print "<br />";
 
 // Print using foreach
+foreach ($data as $list)
+{
+  echo $list['id']." ".$list['title_uri']."<br />";
+}
+
+
+/*
 foreach ($data->posts as $list)
 {
   echo $list['title_uri']."<br />";
 }
+*/
